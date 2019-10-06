a27ca3225b439f3c564b91e4c9e92920e663729d
Samir Trajano Feitosa <samir.feitosa@virtus.ufcg.edu.br>
2019-10-05 21:19:07 -0300

    add safe area on drawer list

--- flutter_basic/lib/drawer_list.dart
+++ flutter_basic/lib/drawer_list.dart
@@ -3,48 +3,50 @@ import 'package:flutter/material.dart';
 class DrawerList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        children: <Widget>[
-          UserAccountsDrawerHeader(
-            accountName: Text("Samirtf"),
-            accountEmail: Text("samirtf@email.com"),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: AssetImage("assets/images/dog1.png"),
-              //Use below to load image through network
-              //backgroundImage: NetworkImage("https://avatars1.githubusercontent.com/u/5253073?s=460&v=4"),
+    return SafeArea(
+      child: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("Samirtf"),
+              accountEmail: Text("samirtf@email.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage("assets/images/dog1.png"),
+                //Use below to load image through network
+                //backgroundImage: NetworkImage("https://avatars1.githubusercontent.com/u/5253073?s=460&v=4"),
+              ),
             ),
-          ),
-          ListTile(
-            leading: Icon(Icons.star),
-            title: Text("Favorites"),
-            subtitle: Text("more info..."),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              print("Item 1");
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.help),
-            title: Text("Help"),
-            subtitle: Text("more info..."),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              print("Item 2");
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text("Logout"),
-            trailing: Icon(Icons.arrow_forward),
-            onTap: () {
-              print("Item 3");
-              Navigator.pop(context);
-            },
-          ),
-        ],
+            ListTile(
+              leading: Icon(Icons.star),
+              title: Text("Favorites"),
+              subtitle: Text("more info..."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print("Item 1");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.help),
+              title: Text("Help"),
+              subtitle: Text("more info..."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print("Item 2");
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text("Logout"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                print("Item 3");
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
